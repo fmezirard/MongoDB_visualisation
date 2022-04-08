1a9ca7fcbef15bde50a472b21b0ba610ee3ec6b3
fmezirard <floriane.mezirard@etudiant.univ-rennes2.fr>
2022-04-08 22:09:00 +0200

    Modification 2.2 ; ajout du traitement pour avoir le dataframe voulu pour le graphe réseau + ajout du code pour le graphe

--- pages/Page2.py
+++ pages/Page2.py
@@ -2,6 +2,11 @@ from pymongo import MongoClient
 import networkx as nx
 import pandas as pd
 import numpy as np
+from bokeh.io import output_file, show
+from bokeh.models import (BoxZoomTool, Circle, HoverTool,
+                          MultiLine, Plot, Range1d, ResetTool)
+from bokeh.palettes import Spectral4
+from bokeh.plotting import from_networkx
 
 # Réseau de publications scientifiques
 
@@ -68,13 +73,61 @@ for publi in auteurspublications:
 data = pd.DataFrame({"nomauteurs": nomauteurs, "prenomauteurs":prenomauteurs})#, "publications":publications})
 print(data)
 
-for nom in data.nomauteurs:
-    publinom = data.publications[nomauteurs == nom]
+nbrepublicommun = []
+noeudcible = []
+noeudsource = []
+for source in data.nomauteurs:
+    publisource = data.publications[nomauteurs == source]
 
-    for nom in data.nomauteurs:
-        publinom = data.publications[nomauteurs == nom]
+    for cible in data.nomauteurs:
+        publicible = data.publications[nomauteurs == cible]
+        
+        cmp = {}
+        publitotale = publisource + publicible
+        for elem in publitotale:
+            cmp["elem"] = cmp.get(elem,0) + 1
+        
+        commun = 0
+        for nbre in cmp.values():
+            if nbre != 1:
+                commun += 1
+
+    noeudsource.append(source)
+    noeudcible.append(cible)
+    nbrepublicommun.append(commun)
         
 
+datagraph = pd.DataFrame({"noeudsource": noeudsource, "noeudcible": noeudcible, "nbrepublicommun": nbrepublicommun})
+
+# Préparation pour le graphique 
+G = nx.datagraph()
+
+# SAME_PUB, DIFFERENT_PUB = "black", "red"
+# edge_attrs = {}
+
+# for start_node, end_node, _ in G.edges(data=True):
+#     edge_color = SAME_PUB if G.nodes[start_node]["pub"] == G.nodes[end_node]["pub"] else DIFFERENT_PUB
+#     edge_attrs[(start_node, end_node)] = edge_color
+
+# nx.set_edge_attributes(G, edge_attrs, "edge_color")
+
+# Graphique du réseau
+plot = Plot(width=400, height=400,
+            x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
+plot.title.text = "Réseau entre les auteurs"
+
+# Ajout d'un outils de survol, identification du noeud
+node_hover_tool = HoverTool(tooltips=[("auteur", "@noeudsource")])
+plot.add_tools(node_hover_tool, BoxZoomTool(), ResetTool())
+
+graph_renderer = from_networkx(G, nx.spring_layout, scale=1, center=(0, 0))
+
+graph_renderer.node_renderer.glyph = Circle(size=15, fill_color=Spectral4[0])
+graph_renderer.edge_renderer.glyph = MultiLine(line_color="edge_color", line_alpha=0.8, line_width=1)
+plot.renderers.append(graph_renderer)
+
+output_file("Page2.html")
+show(plot)
 
 # code MongoDB
 # db.getCollection('hal_irisa_2021').find({"authors.name": {$in : ["Kaim","Canard"]}, 
